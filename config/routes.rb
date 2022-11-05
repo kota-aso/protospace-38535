136d61050e17e1a2343a0b4e1b79307b300cb319
kota-aso <kotaaso1212@gmail.com>
2022-11-05 18:03:00 +0900

    コメント投稿と表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes do
-    resources :comment, only: :create
+    resources :comments, only: :create
   end
 end
